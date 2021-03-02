a868d8e3b3f1b6976434e432996e10751728b435
Blake Bradford <bbradforddesign@gmail.com>
2021-03-01 19:07:24 -0600

    Switched phone formatting to computed. Added phone formatting to edit form

--- assets/views/charitySignUp1.js
+++ assets/views/charitySignUp1.js
@@ -1,6 +1,6 @@
 const app = new Vue({
-  delimiters: ['${', '}'],
-  el: '#app',
+  delimiters: ["${", "}"],
+  el: "#app",
   data: {
     loaded: false,
     errors: [],
@@ -13,102 +13,119 @@ const app = new Vue({
     state: null,
     zip: null,
   },
+  computed: {
+    formPhone: {
+      get() {
+        /*
+            On change, filter any non-nums, and 
+            return formatted phone num for display.
+        */
+        if (this.phone) {
+          const clean = this.phone.replace(/[^\d]/g, "");
+          const cleanLen = clean.length;
+
+          if (cleanLen < 4) {
+            return clean;
+          } else if (cleanLen < 7) {
+            return `(${clean.slice(0, 3)}) ${clean.slice(3)}`;
+          } else {
+            return `(${clean.slice(0, 3)}) ${clean.slice(3, 6)}-${clean.slice(
+              6,
+              10
+            )}`;
+          }
+        }
+      },
+      set(num) {
+        // update phone in data with new input
+        const clean = num.replace(/[^\d]/g, "");
+        if (clean.length <= 10) {
+          this.phone = clean;
+        }
+      },
+    },
+  },
   mounted() {
     this.loaded = true;
-    if (localStorage.getItem('step1')) {
+    if (localStorage.getItem("step1")) {
       try {
-        step1 = JSON.parse(localStorage.getItem('step1'));
-        this.name = step1.name
-        this.contactName = step1.contactName
-        this.email = step1.email
-        this.phone = step1.phone
-        this.address = step1.address
-        this.city = step1.city
-        this.state = step1.state
-        this.zip = step1.zip
-      } catch(e) {
-        localStorage.removeItem('step1')
+        step1 = JSON.parse(localStorage.getItem("step1"));
+        this.name = step1.name;
+        this.contactName = step1.contactName;
+        this.email = step1.email;
+        this.phone = step1.phone;
+        this.address = step1.address;
+        this.city = step1.city;
+        this.state = step1.state;
+        this.zip = step1.zip;
+      } catch (e) {
+        localStorage.removeItem("step1");
       }
     }
   },
-  methods:{
+  methods: {
     saveStep() {
-      localStorage.setItem('step1', JSON.stringify({
-        name: this.name,
-        contactName: this.contactName,
-        email: this.email,
-        // filter out non-num chars on submission
-        phone: this.phone.replace(/[^\d]/g, ""),
-        address: this.address,
-        city: this.city,
-        state: this.state,
-        zip: this.zip
-      }))
+      localStorage.setItem(
+        "step1",
+        JSON.stringify({
+          name: this.name,
+          contactName: this.contactName,
+          email: this.email,
+          phone: this.phone,
+          address: this.address,
+          city: this.city,
+          state: this.state,
+          zip: this.zip,
+        })
+      );
     },
     checkForm: function (e) {
       e.preventDefault();
 
-      if (this.name &&
-          this.contactName &&
-          this.email &&
-          this.phone &&
-          this.address &&
-          this.city &&
-          this.state && this.state != "Choose..." &&
-          this.zip) {
-        this.saveStep()
-        window.location.assign("/charity/signup/step/2")
+      if (
+        this.name &&
+        this.contactName &&
+        this.email &&
+        this.phone &&
+        this.address &&
+        this.city &&
+        this.state &&
+        this.state != "Choose..." &&
+        this.zip
+      ) {
+        this.saveStep();
+        window.location.assign("/charity/signup/step/2");
         return true;
       }
 
       this.errors = [];
 
       if (!this.name) {
-        this.errors.push('Organization Name required.');
+        this.errors.push("Organization Name required.");
       }
       if (!this.contactName) {
-        this.errors.push('Contact Name required.');
+        this.errors.push("Contact Name required.");
       }
       if (!this.email) {
-        this.errors.push('Email required.');
+        this.errors.push("Email required.");
       }
       if (!this.phone) {
-        this.errors.push('Phone required.');
+        this.errors.push("Phone required.");
       }
       if (!this.address) {
-        this.errors.push('Address required.');
+        this.errors.push("Address required.");
       }
       if (!this.city) {
-        this.errors.push('City required.');
+        this.errors.push("City required.");
       }
       if (this.state == "Choose...") {
-        this.errors.push('State required.');
+        this.errors.push("State required.");
       }
       if (!this.zip) {
-        this.errors.push('Zip required.');
+        this.errors.push("Zip required.");
       }
 
-      console.log(this.errors.length)
-    }
+      console.log(this.errors.length);
+    },
   },
-  watch: {
-      phone: function(num) {
-        /*
-            On change, filter any non-nums, and 
-            return formatted phone num for display.
-        */
-        const clean = num.replace(/[^\d]/g, "")
-        const cleanLen = clean.length
-
-        if (cleanLen < 4) {
-            this.phone = clean;
-        } else if (cleanLen < 7) {
-            this.phone = `(${clean.slice(0, 3)}) ${clean.slice(3)}`
-        } else {
-            this.phone = `(${clean.slice(0,3)}) ${clean.slice(3, 6)}-${clean.slice(6, 10)}`
-        }
-        
-        return this.phone
-    }
-  }
-})
+});
